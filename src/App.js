8b281d3a8b8b48b0d0de239fef27c57a68fa510f
ToritseTuedor <torisevalerie@gmail.com>
2023-05-18 00:34:29 -0400

    deleted files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,1 @@
-import SearchBar from "./components/SearchBar.js";
-import CategoryButtons from "./components/CategoryButtons.js";
-import EventResults from "./components/EventResults";
-import NoResults from "./components/NoResults";
-import axios from "axios";
-import { useState} from "react";
-import './App.css';
 
-function App() {
-  return (
-    <> 
-    <CategoryButtons/>
-    </>
-    
-  );
-}
-
-export default App;
